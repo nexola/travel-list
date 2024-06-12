83e2e57bd1f723da93c0cc905cadbb574af3bd59
nexola <vitormatheusfv@gmail.com>
2024-06-12 16:55:09 -0300

    Travel list

---
+++ src/App.js
@@ -0,0 +1,160 @@
+import { useState } from "react";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Status items={items} />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏖️ Far Away 🧳</h1>;
+}
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
+    console.log(newItem);
+
+    onAddItems(newItem);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>O que precisa pra sua viagem? 🤔</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>ADICIONAR</button>
+    </form>
+  );
+}
+
+function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Ordenar por Ordem de entrada</option>
+          <option value="description">Ordenar por Ordem alfabética</option>
+          <option value="packed">Ordenar por Status do item</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={item.packed}
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      ></input>
+      <span style={{ textDecoration: item.packed ? "line-through" : "none" }}>
+        {item.description} {item.quantity}x
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
+
+function Status({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Comece adicionando alguns itens a sua lista 🚀</em>
+      </p>
+    );
+  }
+  const totalItems = items.length;
+  const itemsPacked = items.filter((item) => item.packed).length;
+  const percentagePacked = Math.floor((itemsPacked * 100) / totalItems);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentagePacked === 100
+          ? "Pegou tudo! Vambora ✈️"
+          : `💼 Você tem ${totalItems} itens na sua lista e já pegou
+        ${itemsPacked} (${percentagePacked}%)`}
+      </em>
+    </footer>
+  );
+}
